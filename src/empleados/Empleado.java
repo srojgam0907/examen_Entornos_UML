6075bc0412a8e1b2c62a76291411e6f8eb0426f3
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-11 08:22:19 +0100

    Primer commit

---
+++ src/empleados/Empleado.java
@@ -0,0 +1,5 @@
+package empleados;
+
+public abstract class Empleado {
+
+}
---
+++ src/empleados/Main.java
@@ -0,0 +1,10 @@
+package empleados;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
