9506a026a22fa11c42a6999ad64aa777b562442d
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-11 08:59:44 +0100

    Clase Empleado hecha

--- src/empleados/Empleado.java
+++ src/empleados/Empleado.java
@@ -2,4 +2,37 @@ package empleados;
 
 public abstract class Empleado {
 
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private double sueldoBase;
+	
+	public Empleado(String dni, String nombre, String apellidos, double sueldoBase) {
+		this.dni= dni;
+		this.nombre= nombre;
+		this.apellidos= apellidos;
+		this.sueldoBase= sueldoBase;
+	}
+
+	public double getSueldoBase() {
+		return sueldoBase;
+	}
+
+	public void setSueldoBase(double sueldoBase) {
+		this.sueldoBase = sueldoBase;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+	
+	public abstract double getSueldo(); 
 }
