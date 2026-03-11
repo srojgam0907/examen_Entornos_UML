716e5f132035a349cba171107763abb689bbcdd5
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-11 08:46:33 +0100

    Interfaz IEntradaSalida hecha

--- src/empleados/IEntradaSalida.java
+++ src/empleados/IEntradaSalida.java
@@ -2,4 +2,19 @@ package empleados;
 
 public interface IEntradaSalida {
 
+	public void imprimir(String texto);
+	
+	public void imprimirLinea(String texto);
+	
+	public String leerTexto(String mensaje);
+	
+	public int leerEntero(String mensaje);
+	
+	public double leerImporte(String mensaje);
+	
+	public void mostrarMenu();
+	
+	public void limpiarPantalla();
+	
+	public void pausa();
 }
