52bf42aeeef90c3525b893249bbb323857d05907
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-11 08:42:59 +0100

    Clase Comercial hecha

--- src/empleados/Comercial.java
+++ src/empleados/Comercial.java
@@ -2,4 +2,22 @@ package empleados;
 
 public class Comercial {
 
+	private double ventas;
+	private double sueldoBase;
+	
+	public Comercial(String nombre, String apellidos, double sueldoBase, double ventas) {
+		nombre= nombre;
+		apellidos= apellidos;
+		this.sueldoBase= sueldoBase;
+		this.ventas= ventas;
+	}
+	
+	public void setVentas(double ventas) {
+		this.ventas= ventas;
+	}
+	
+	public double getSueldo() {
+		
+		return sueldoBase + (ventas * 0.10); 
+	}
 }
