beba6aecd0f7ae5140147a2d6cb7cc75add39da2
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-11 09:51:57 +0100

    Clase Plantilla hecha

--- src/empleados/Plantilla.java
+++ src/empleados/Plantilla.java
@@ -1,5 +1,40 @@
 package empleados;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Plantilla {
 
+	private List<Empleado> empleados;
+	
+	public Plantilla() {
+		this.empleados= new ArrayList<>();
+	}
+	
+	public void agregarEmpleado(Empleado empleado) {
+		empleados.add(empleado);
+	}
+	
+	public List<Empleado> getEmpleadosPorNombre(String filtroNombre) {
+		List<Empleado> empleadosFiltro= new ArrayList<>();
+		String filtro= filtroNombre.trim().toLowerCase();
+		
+		
+		if(filtroNombre == "") {
+			empleadosFiltro.addAll(this.empleados); 
+		
+		} else {
+			for(int i=0; i< empleados.size(); i++) {
+				Empleado empleado= empleados.get(i);
+				String nombreEmpleado= (empleado.getNombre() + "" + empleado.getApellidos()).toLowerCase();
+				
+				
+				if(nombreEmpleado.contains(filtro)) {
+					empleadosFiltro.add(empleado);
+				}
+			}
+		}
+		
+		return empleadosFiltro;
+	}
 }
