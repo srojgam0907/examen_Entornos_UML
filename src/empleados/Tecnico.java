27bd20a3ab492afb44cb3e11dce325cd15500736
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-11 08:33:56 +0100

    Clase Tecnico hecha

--- src/empleados/Tecnico.java
+++ src/empleados/Tecnico.java
@@ -1,5 +1,21 @@
 package empleados;
 
 public class Tecnico {
-
+	
+	private int categoria;
+	String dni;
+	String apellidos;
+	double sueldoBase;
+	
+	public Tecnico(String dni, String apellidos, double sueldoBase, int categoria) {
+		this.dni= dni; 
+		this.apellidos= apellidos;
+		this.sueldoBase= sueldoBase;
+		this.categoria= categoria;
+	}
+	
+	public double getSueldo() {
+		
+		return sueldoBase + (categoria*100); 
+	}
 }
