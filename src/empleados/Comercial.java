1e69951debda5df4362e5925b544c363ba90376a
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-11 09:18:49 +0100

    Correccion en el constructor del Comercial

--- src/empleados/Comercial.java
+++ src/empleados/Comercial.java
@@ -4,9 +4,8 @@ public class Comercial extends Empleado{
 
 	private double ventas;
 	
-	public Comercial(String dni, String nombre, String apellidos, double sueldoBase, double ventas) {
+	public Comercial(String dni, String nombre, String apellidos, double sueldoBase) {
 		super(dni, nombre, apellidos, sueldoBase); 
-		this.ventas= ventas;
 	}
 	
 	public void setVentas(double ventas) {
