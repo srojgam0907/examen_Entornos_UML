8b824eed5daa0f6c53f5ddc97d24d9b298066229
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-11 10:10:35 +0100

    Clase GestorEmpleados sin terminar

--- src/empleados/GestorEmpleados.java
+++ src/empleados/GestorEmpleados.java
@@ -1,5 +1,52 @@
 package empleados;
 
+import java.util.List;
+
 public class GestorEmpleados {
 
+	private Plantilla plantilla;
+	private IEntradaSalida consola;
+	
+	public GestorEmpleados(Plantilla plantilla, IEntradaSalida consola) {
+		this.plantilla= new Plantilla();
+		this.consola= new Consola();
+	}
+	
+	public void ejecutar() {
+		boolean salir= false;
+		
+		do {
+			consola.mostrarMenu();
+			
+			
+			
+		} while(!salir);
+	}
+	
+	private void contratarEmpleado() {
+		
+	}
+	
+	private void listarTodos() {
+		
+	}
+	
+	private void listarPorFiltro() {
+		List<Empleado> empleados;
+		
+		empleados= plantilla.getEmpleadosPorNombre(consola.leerTexto("Introduce el filtro para iniciar la busqueda"));
+		
+		for(int i=0; i<empleados.size(); i++) {
+			consola.imprimir(i+1 + " - " + empleados.get(i).getNombre() + " " + empleados.get(i).getApellidos() + ": " + empleados.get(i).getSueldoBase() + " €");
+			
+		}
+	}
+	
+	private void listarEmpleados(List<Empleado> empleados) {
+		
+	}
+	
+	private void ordenarPorNombre(List<Empleado> empleados) {
+		
+	}
 }
